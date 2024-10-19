0d31e046e7d09b52e0378753c9bd8a4f4649bee7
William Liu <orangetnt827@gmail.com>
2024-10-19 14:12:46 -0500

    fix link

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <div class="text-2xl h-auto p-4 bg-gray-600 text-center w-screen flex flex-col sm:flex-row items-center justify-center text-white">
   <div class="sm:space-x-8 flex flex-col sm:flex-row items-center">
-    <a class="cursor-pointer text-white uppercase" href="{base}">Home</a>
+    <a class="cursor-pointer text-white uppercase" href="{base}/">Home</a>
     <a class="cursor-pointer text-white uppercase" href="{base}/resources">Resources</a>
     <a class="cursor-pointer text-white uppercase" href="{base}/events">Events</a>
     <a class="cursor-pointer text-white uppercase" href="{base}/team">Team</a>
