7e4f810dd58386f15e7f8daaa2123d5d2f19983f
William Liu <orangetnt827@gmail.com>
2024-10-19 14:05:30 -0500

    add specifier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
   <div slot="content">
     <h1 class="text-5xl text-center my-12 text-blue-500">San Antonio Math Circle</h1>
     <h2 class="text-3xl my-4">Mission Statement</h2>
-    <p>The mission of the San Antonio Math Circle is to identify and develop exceptional high school math students.</p>
+    <p>The mission of the San Antonio Math Circle is to identify and develop exceptional high school math students in San Antonio.</p>
     <h2 class="text-3xl my-4">About the Circle</h2>
     <p>The Circle is a place for students interested in mathematics to interact with one another and to learn math, solve problems and participate in math competitions.
     We strive to engage and encourage all interested students to join this highly enriched community in which like-minded, interested students deepen their appreciation, understanding and enjoyment of Mathematics.</p>
