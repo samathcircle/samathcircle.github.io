d776d20ed102e86303ee7d2e91e51aaaf0e49454
William <123657188+renaissancedog@users.noreply.github.com>
2024-10-20 12:06:15 -0500

    break words on links

--- src/lib/components/Resource.svelte
+++ src/lib/components/Resource.svelte
@@ -2,6 +2,6 @@
   export let name, link, desc;
 </script>
 <div class="my-4 w-full">
-  <h2 class='py-2 text-xl'>{name} <a href={link} target="_blank" class="text-sm underline text-blue-600">{link}</a></h2>
+  <h2 class='py-2 text-xl'>{name} <a href={link} target="_blank" class="text-sm underline text-blue-600 break-words">{link}</a></h2>
   <p class='mb-2'>{desc}</p>
 </div>
