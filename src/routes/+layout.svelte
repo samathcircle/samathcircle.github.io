4f0273717ec1874d880f4052f0229be1bc762943
William Liu <orangetnt827@gmail.com>
2024-12-14 10:11:03 -0600

    google search console stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
         name="description"
         content="The website of the San Antonio Math Circle"
     />
+    <meta name="google-site-verification" content="zc2wucogeYpcjEVzrefVoU5s6OcVqCdJuWL6PPWiQO4" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
